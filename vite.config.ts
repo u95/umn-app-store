887e2323755fbac1aa23c3f93e2bd45b8ee6dd04
u95 <86998025+u95@users.noreply.github.com>
2026-07-13 16:17:45 +0530

    build: update base path for GitHub Actions
    Use the GITHUB_ACTIONS environment variable instead of the command type to determine the base path for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
-    base: command === 'build' ? '/umn-appstore/' : '/',
+    base: process.env.GITHUB_ACTIONS === 'true' ? '/umn-appstore/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
